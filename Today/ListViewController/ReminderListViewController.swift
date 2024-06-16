84342b26bc00706505fef5d3df43ecd5bf903fe6
Anurag Tyagi <anurag.tyagi4964@gmail.com>
2024-06-16 12:06:46 +0530

    feat: displaying reminder details

--- Today/ListViewController/ReminderListViewController.swift
+++ Today/ListViewController/ReminderListViewController.swift
@@ -26,6 +26,21 @@ class ReminderListViewController: UICollectionViewController {
 
         collectionView.dataSource = dataSource
     }
+    
+    override func collectionView(
+           _ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath
+    ) -> Bool {
+        let id = reminders[indexPath.item].id
+        pushDetailViewForReminder(withId: id)
+        return false
+    }
+    
+    func pushDetailViewForReminder(withId id: Reminder.ID){
+        let reminder = reminder(withId: id)
+        let reminderViewController = ReminderViewController(reminder: reminder)
+        navigationController?
+            .pushViewController(reminderViewController, animated:true)
+    }
 
     private func listLayout() -> UICollectionViewCompositionalLayout {
         var listConfiguration = UICollectionLayoutListConfiguration(appearance: .grouped)
