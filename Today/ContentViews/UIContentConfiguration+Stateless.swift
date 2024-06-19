e58a07b567a6a45f673682795bcb57b2bef3ad1b
Anurag Tyagi <anurag.tyagi4964@gmail.com>
2024-06-19 15:10:44 +0530

    feat: added editing view and flow

---
+++ Today/ContentViews/UIContentConfiguration+Stateless.swift
@@ -0,0 +1,14 @@
+//
+//  UIContentConfiguration+Stateless.swift
+//  Today
+//
+//  Created by Anurag Tyagi on 18/06/24.
+//
+
+import UIKit
+
+extension UIContentConfiguration {
+    func updated(for state: UIConfigurationState) -> Self {
+        return self
+    }
+}
